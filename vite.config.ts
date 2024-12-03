383ae1dcf716c7f180654b22d435ef9bef030c3e
development-hobby <jesusalcalarojas@gmail.com>
2024-12-03 17:53:21 -0400

    chore: update configuration build library

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,39 @@
 /// <reference types="vitest" />
 /// <reference types="Vite/client" />
-import { defineConfig } from 'vite'
-import { resolve } from 'node:path'
-import dts from 'vite-plugin-dts'
+import { defineConfig } from "vite";
+import { resolve } from "node:path";
+import dts from "vite-plugin-dts";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [dts({
-    rollupTypes: true,
-  })],
+  plugins: [
+    dts({
+      rollupTypes: true,
+    }),
+  ],
   build: {
-    outDir: 'lib',
+    outDir: "dist",
+    minify: true,
+    rollupOptions: {
+      treeshake: true,
+      external: ["react", "react-dom", "react/jsx-runtime"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+          "react/jsx-runtime": "jsxRuntime",
+        },
+      },
+    },
     lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
-      name: 'fluwtate',
-      fileName: 'index',
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "fluwtate",
+      fileName: "index",
     },
   },
   test: {
     globals: true,
     clearMocks: true,
-    environment: 'happy-dom',
+    environment: "happy-dom",
   },
-})
+});
